54b5fd4b6404c911b40eace27d596cd44991fda0
David Ryan Alviola <david.alviola@gmail.com>
2021-04-14 10:44:31 -0500

    Added comments to functions.

--- acquire.py
+++ acquire.py
@@ -4,6 +4,10 @@ import requests as req
 from io import StringIO
 
 def _create_df_from_payloads(endpoint, max_pages, target_key_name):
+    """
+    Helper function that loops through the pages returned from the Zach API,\
+    adds the information to a list and then converts to a single dataframe that is returned.
+    """
     page_list = []
     
     for i in range(1, max_pages + 1):
@@ -15,6 +19,10 @@ def _create_df_from_payloads(endpoint, max_pages, target_key_name):
     return pd.DataFrame(page_list)
 
 def acquire_df_from_zach_api(endpoint, target_key_name):
+    """
+    Takes in the api endpoint and desired key (items, stores, sales) and\
+    returns a dataframe with the data from the response.
+    """
     response = req.get(endpoint)
     response = response.json()
 
@@ -23,6 +31,12 @@ def acquire_df_from_zach_api(endpoint, target_key_name):
     return _create_df_from_payloads(endpoint, max_pages, target_key_name)
 
 def merge_zach_dataframes(items_df, stores_df, sales_df):
+    """
+    Takes in the three data frames containing the information from the Zach API,\
+    renames 'item' and 'store' columns from the sales_df to match the foreign keys\
+    on the items_df and stores_df, then peforms left joins using those foreign keys.\
+    Returns a single data frame containing the information from the original three.
+    """
     merged_df = pd.DataFrame()
     
     sales_df.rename(columns={'item' : 'item_id'}, inplace=True)
@@ -34,6 +48,9 @@ def merge_zach_dataframes(items_df, stores_df, sales_df):
     return merged_df
 
 def acquire_germany():
+    """
+    Returns a dataframe containing Open Power Systems data for Germany.
+    """
     response = req.get("https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv")
     csv = StringIO(response.text)
     
